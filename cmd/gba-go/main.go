9a6a0e72d1b79b309510e4cc413758a3ef4dfe4e
Satoshi Warita <waritasa@ikyu.com>
2025-06-27 21:52:06 +0900

    add ebitengine

--- cmd/gba-go/main.go
+++ cmd/gba-go/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+const (
+	// GBA screen dimensions
+	screenWidth  = 240
+	screenHeight = 160
+	// Scale factor for display
+	scaleFactor = 3
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "GBA Emulator - Under Development")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	ebiten.SetWindowSize(screenWidth*scaleFactor, screenHeight*scaleFactor)
+	ebiten.SetWindowTitle("GBA Emulator")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 }
